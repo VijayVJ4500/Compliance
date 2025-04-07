4e12a1a6f0632841d06b34cdf57b5bd8bb679f70
vijay A <154329392+VijayVJ4500@users.noreply.github.com>
2025-04-07 13:18:06 +0530

    Update test_calculator.py

--- Tests/test_calculator.py
+++ Tests/test_calculator.py
@@ -1,5 +1,8 @@
 import pytest
-from src.calculator import Calculator
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+from calculator import Calculator
 
 @pytest.fixture
 def calc():
