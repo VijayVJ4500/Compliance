be76e0ce17d5e79256385f1ca90bc30d5b406094
VijayVJ4500 <Vijay.a@techgenzi.com>
2025-04-07 16:56:54 +0530

    Hrm Automation

--- hrm_automation/hrm_test_cases/Idcard_detail.py
+++ hrm_automation/hrm_test_cases/Idcard_detail.py
@@ -3,6 +3,7 @@ import unittest
 from hrm_automation.base import BasePage
 from hrm_automation.locators import Locators
 import logging
+from logger_file import logger
 import hrm_automation.logger_file
 
 # # Configure logger
